2fe087a6d44642db71cf7adfb2126448eeb41531
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-10-13 01:32:25 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 //my urls
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','article-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }
 );
 
