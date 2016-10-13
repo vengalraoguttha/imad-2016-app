54ebec7fffe019c57a4b5c4d07205ad42c0f6d60
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-10-13 01:09:06 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+//my urls
+app.get('/article-one',function(req,res){
+    res.send("atricle one requested and will be served here");
+}
+);
+
+app.get('/article-two',function(req,res){
+    res.send("atricle two requested and will be served here");
+}
+);
+
+app.get('/article-three',function(req,res){
+    res.send("atricle three requested and will be served here");
+}
+);
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
