9c99674879baa2d178caf98013c779ba1304f5ee
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-05 21:57:19 -0700

    [imad-console] Updates ui/registrationjs.js

--- ui/registrationjs.js
+++ ui/registrationjs.js
@@ -27,6 +27,8 @@ loadRegistration(){
         // Make the request
             var username = document.getElementById('username').value;
             var password = document.getElementById('password').value;
+            console.log(username);
+            console.log(password);
             request.open('POST', '/login', true);
             request.setRequestHeader('Content-Type', 'application/json');
             request.send(JSON.stringify({username: username, password: password}));  
