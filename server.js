0cf3e9b613d2006507480bdae78276c8cbdeeb16
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-05 23:43:10 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -247,6 +247,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  console.log("check");
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
