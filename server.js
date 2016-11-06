b924f2accddd7afa488779b3fffd8483e4d2962c
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-05 23:34:15 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -247,18 +247,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
-    if (err) {
-        res.status(500).send(err.toString());
-    } else {
-        if (result.rows.length === 0) {
-            res.status(404).send('Article not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-        }
-    }
-  });
+  
 });
 
 app.get('/ui/:fileName', function (req, res) {
