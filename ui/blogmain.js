20c7e8c3c496234c585153989d646ec193ca936e
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-05 23:17:43 -0700

    [imad-console] Updates ui/blogmain.js

--- ui/blogmain.js
+++ ui/blogmain.js
@@ -61,4 +61,33 @@ function loadLogin () {
     request.open('GET', '/check-login-blog', true);
     request.send(null);
 }
+
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
 loadlogin();
+
+loadArticles();
