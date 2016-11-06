a0770b38f502c48c2f77b7351398c64283f9b10f
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-05 22:34:49 -0700

    [imad-console] Updates ui/blogmain.js

--- ui/blogmain.js
+++ ui/blogmain.js
@@ -1,38 +1,40 @@
-
-var username=document.getElementById("username");
-var password=document.getElementById("password");
-var submit=document.getElementById("btn_login");
-
-submit.onclick=function(){
-    var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  submit.value = 'Sucess!';
-              } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
-              } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              } else {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              }
-              loadLogin();
-          }  
-          // Not done yet
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/login-blog', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logging in...';
-};
+function loadlogin(){
+    var username=document.getElementById("username");
+    var password=document.getElementById("password");
+    var submit=document.getElementById("btn_login");
+    
+    submit.onclick=function(){
+        var request = new XMLHttpRequest();
+            
+            // Capture the response and store it in a variable
+            request.onreadystatechange = function () {
+              if (request.readyState === XMLHttpRequest.DONE) {
+                  // Take some action
+                  if (request.status === 200) {
+                      submit.value = 'Sucess!';
+                  } else if (request.status === 403) {
+                      submit.value = 'Invalid credentials. Try again?';
+                  } else if (request.status === 500) {
+                      alert('Something went wrong on the server');
+                      submit.value = 'Login';
+                  } else {
+                      alert('Something went wrong on the server');
+                      submit.value = 'Login';
+                  }
+                  loadLogin();
+              }  
+              // Not done yet
+            };
+            
+            // Make the request
+            var username = document.getElementById('username').value;
+            var password = document.getElementById('password').value;
+            console.log(username);
+            console.log(password);
+            request.open('POST', '/login-blog', true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify({username: username, password: password}));  
+            submit.value = 'Logging in...';
+    };
+}
+loadlogin();
