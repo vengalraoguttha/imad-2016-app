c57eaa07fa311f11628d34fa319ba03c2f10f8b8
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-05 23:48:11 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -117,7 +117,7 @@ function loadArticles () {
                 for (var i=0; i< articleData.length; i++) {
                     content += `<li>
                     <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li>`;
+                    </li>`;
                 }
                 content += "</ul>"
                 articles.innerHTML = content;
