174e496b7cec871df8a9b64554078159203887a8
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-07 19:43:20 +0530

    [imad-console] Updates ui/writeArticlejs.js

--- ui/writeArticlejs.js
+++ ui/writeArticlejs.js
@@ -1,1 +1,9 @@
 
+var submit=document.getElementById("publish");
+var title=document.getElementById("title");
+var head=document.getElementById("heading");
+var content=document.getElementById("article");
+
+submit.onclick=function(){
+    
+};
