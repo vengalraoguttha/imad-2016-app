62fecf2988bb9dd17338050fd88c70c2aac1ec84
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-07 19:52:27 +0530

    [imad-console] Updates ui/writeArticlejs.js

--- ui/writeArticlejs.js
+++ ui/writeArticlejs.js
@@ -5,7 +5,35 @@ function writeArticle(){
     var content=document.getElementById("article");
     
     submit.onclick=function(){
-        
+        var request = new XMLHttpRequest();
+            
+            // Capture the response and store it in a variable
+            request.onreadystatechange = function () {
+              if (request.readyState === XMLHttpRequest.DONE) {
+                  // Take some action
+                  if (request.status === 200) {
+                      submit.value = 'Sucess!';
+                  } else if (request.status === 403) {
+                      submit.value = 'Invalid credentials. Try again?';
+                  } else if (request.status === 500) {
+                      alert('Something went wrong on the server');
+                      submit.value = 'Login';
+                  } else {
+                      alert('Something went wrong on the server');
+                      submit.value = 'Login';
+                  }
+                  loadLogin();
+              }  
+              // Not done yet
+            };
+            
+            // Make the request
+            var username = document.getElementById('username').value;
+            var password = document.getElementById('password').value;
+            request.open('POST', '/login-blog', true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify({username: username, password: password}));  
+            submit.value = 'Logging in...';
     };
 }
 
