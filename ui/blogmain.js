2ef67923070ba31fc370eb60bf4ab3c46e4dae03
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-07 19:51:05 +0530

    [imad-console] Updates ui/blogmain.js

--- ui/blogmain.js
+++ ui/blogmain.js
@@ -39,8 +39,6 @@ function loadLoginForm(){
             // Make the request
             var username = document.getElementById('username').value;
             var password = document.getElementById('password').value;
-            console.log(username);
-            console.log(password);
             request.open('POST', '/login-blog', true);
             request.setRequestHeader('Content-Type', 'application/json');
             request.send(JSON.stringify({username: username, password: password}));  
