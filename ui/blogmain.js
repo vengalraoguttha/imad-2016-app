6dec0c8075344ddaacf332c2f03bf26353360bc4
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-07 19:52:28 +0530

    [imad-console] Updates ui/blogmain.js

--- ui/blogmain.js
+++ ui/blogmain.js
@@ -21,7 +21,7 @@ function loadLoginForm(){
                   // Take some action
                   if (request.status === 200) {
                       submit.value = 'Sucess!';
-                      request.open('GET','/blog-main',true);
+                     
                   } else if (request.status === 403) {
                       submit.value = 'Invalid credentials. Try again?';
                   } else if (request.status === 500) {
