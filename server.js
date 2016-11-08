63a15d1b6b1d5e01e59c7ae3f991c0d896704d16
vengalraoguttha <vengalraoguttha@users.noreply.github.com>
2016-11-08 19:59:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ function createTemplate (data) {
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
 });
 
 
